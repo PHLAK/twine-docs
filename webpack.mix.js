86ef36d6ba65dadacbaeba131f39599419a64e57
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-09-09 19:58:55 -0700

    Added PurgeCSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 let mix = require('laravel-mix');
 let build = require('./tasks/build.js');
 let tailwindcss = require('tailwindcss');
+require('laravel-mix-purgecss');
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
@@ -21,5 +22,11 @@ mix.copy('source/_assets/js/asciinema-player.js', 'source/assets/build/js');
 
 mix.sass('source/_assets/sass/tailwind.scss', 'css/app.css').options({
     processCssUrls: false,
-    postCss: [ tailwindcss('./tailwind.config.js') ],
+    postCss: [tailwindcss('tailwind.config.js')],
 }).version();
+
+mix.purgeCss({
+    folders: ['source'],
+    whitelist: ['html', 'body', 'code', 'pre'],
+    whitelistPatterns: [/^fa\-/]
+});
